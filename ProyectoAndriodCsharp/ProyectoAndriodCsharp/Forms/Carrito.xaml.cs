a8842858fc74ec7434d8c3f49d1637618949acc8
Michael Steven Monge Mora <mmongem054@ulacit.ed.cr>
2020-11-06 14:47:08 -0600

    Se agrego el Form de carrito

---
+++ ProyectoAndriodCsharp/ProyectoAndriodCsharp/Forms/Carrito.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ProyectoAndriodCsharp.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoAndriodCsharp.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Carrito : ContentPage
+    {
+        public Carrito()
+        {
+            InitializeComponent();
+        }
+
+        private void Volver_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new MenuPrincipal());
+        }
+
+        private void Logout_Clicked(object sender, EventArgs e)
+        {
+            Memoria.UsuarioActual = null;
+            Application.Current.MainPage = new NavigationPage(new LoginRegistro());
+        }
+    }
+}
