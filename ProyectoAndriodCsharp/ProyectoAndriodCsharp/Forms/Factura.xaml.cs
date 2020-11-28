f93e42fc756905b67bc9ab9820f13a0fe7dd84b5
Adrian <adriancampos2001@gmail.com>
2020-11-27 18:59:38 -0600

    Update Factura
    Modificada la factura para que agarre los datos del archivo de texto

--- ProyectoAndriodCsharp/ProyectoAndriodCsharp/Forms/Factura.xaml.cs
+++ ProyectoAndriodCsharp/ProyectoAndriodCsharp/Forms/Factura.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using ProyectoAndriodCsharp.Controller;
 using ProyectoAndriodCsharp.Model;
-using ProyectoAndriodCsharp.Models;
 using ProyectoAndriodCsharp.Objects;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Markup;
+using Xamarin.Forms.Markup.LeftToRight;
 using Xamarin.Forms.Xaml;
 
 namespace ProyectoAndriodCsharp.Forms
@@ -14,30 +20,29 @@ namespace ProyectoAndriodCsharp.Forms
         public Factura()
         {
             InitializeComponent();
-            btnVerFactura.Clicked += BtnVerFactura_Clicked;
-
-            listaProductos.ItemTapped += ListaProductos_ItemTapped;
+            lblCliente.Text = "Cliente: " + Memoria.UsuarioActual.Nombre;
+            lblEmail.Text = "Correo Registrado: " + Memoria.UsuarioActual.Email;
+            decimal cobroTotal = 0;
+            int count = 0;
+            if (Memoria.UsuarioActual.US_ROL.Equals("Usuario"))
+            {
+                foreach (var product in Memoria.listaCarrito)
+                {
+                    listaProductos.Children.Add(new Label { Text = ProductoRepository.GetProductoByID(product.PRO_ID).PRO_NOMBRE }, 0, count);
+                    listaProductos.Children.Add(new Label { Text = "c/u $" + Math.Truncate(ProductoRepository.GetProductoByID(product.PRO_ID).PRO_PRECIO).ToString() }, 1, count);
+                    listaProductos.Children.Add(new Label { Text = "Cantidad: " + product.COMP_CANTIDAD.ToString() }, 2, count);
+                    cobroTotal = cobroTotal + ((ProductoRepository.GetProductoByID(product.PRO_ID).PRO_PRECIO) * product.COMP_CANTIDAD);
+                    count += 1;
+                }
+            }
+            lblTotal.Text = "Total: $" + cobroTotal;
             btnSalir.Clicked += BtnSalir_Clicked;
         }
-        private void ListaProductos_ItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            DisplayAlert("Producto:", listaProductos.SelectedItem.ToString(), "OK");
-        }
+
 
         private void BtnSalir_Clicked(object sender, EventArgs e)
         {
-
             Application.Current.MainPage = new NavigationPage(new LoginRegistro());
-        }
-
-
-
-        private void BtnVerFactura_Clicked(object sender, EventArgs e)
-        {
-            lblCliente.Text = "Cliente: " + Memoria.UsuarioActual.Nombre;
-            lblEmail.Text = "Correo Registrado: " + Memoria.UsuarioActual.Email;
-            listaProductos.IsVisible = true;
-            lblTotal.Text = "Monto Total a Pagar: $4450";
         }
     }
 }
